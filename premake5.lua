9276ab8c5c79cdba8852fa1fe583b972aef48fa0
Tim van de Wetering <45339455+timeester3648@users.noreply.github.com>
2022-12-07 15:23:23 +0100

    Update submodule

---
+++ premake5.lua
@@ -0,0 +1,17 @@
+include "../../premake/common_premake_defines.lua"
+
+project "magic_enum"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++latest"
+	cdialect "C17"
+	targetname "%{prj.name}"
+	inlining "Auto"
+
+	files {
+		"./**.hpp",
+	}
+
+	includedirs {
+		"%{IncludeDir.magic_enum}"
+	}
